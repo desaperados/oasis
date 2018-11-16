025325fa1fdf0190a54a502156a0de161f4542d8
dc <dc@log-ix.com>
2018-11-16 03:25:07 +0000

    Display offers

--- data/schema.sql
+++ data/schema.sql
@@ -8,23 +8,9 @@ CREATE TABLE IF NOT EXISTS public.offer (
   era     integer
 );
 
-CREATE SCHEMA IF NOT EXISTS api;
-
-CREATE OR REPLACE VIEW api.offer AS (
-  SELECT
-    o.id,
-    o.lot,
-    o.lotGem,
-    o.bid,
-    o.bidGem,
-    o.maker,
-    o.era
-  FROM public.offer o
-);
-
 CREATE SCHEMA IF NOT EXISTS dict;
 
-DROP TABLE IF EXISTS dict.market;
+DROP TABLE IF EXISTS dict.market CASCADE;
 
 CREATE TABLE dict.market (
   id    character varying(10) primary key,
@@ -37,7 +23,7 @@ INSERT INTO dict.market (id, base, quote) VALUES
   ('MKRDAI', 'MKR', 'DAI'),
   ('MKRWETH', 'MKR', 'WETH');
 
-DROP TABLE IF EXISTS dict.token;
+DROP TABLE IF EXISTS dict.token CASCADE;
 
 CREATE TABLE dict.token (
   symbol   character varying(5) unique not null,
@@ -47,6 +33,41 @@ CREATE TABLE dict.token (
 );
 
 INSERT INTO dict.token (symbol, key, decimals, chain) VALUES
-  ('WETH', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'mainnet'),
-  ('DAI',  '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359', 18, 'mainnet'),
-  ('MKR',  '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18, 'mainnet');
+  ('WETH', 'C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'mainnet'),
+  ('DAI',  '89d24A6b4CcB1B6fAA2625fE562bDD9a23260359', 18, 'mainnet'),
+  ('MKR',  '9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18, 'mainnet');
+
+CREATE SCHEMA IF NOT EXISTS api;
+
+CREATE OR REPLACE VIEW api.offer AS (
+  SELECT
+    o.id,
+    o.lot,
+    o.lotGem,
+    o.bid,
+    o.bidGem,
+    o.maker,
+    o.era,
+    COALESCE(lotToken.symbol, 'XXX') AS lotSymbol,
+    COALESCE(bidToken.symbol, 'XXX') AS bidSymbol,
+    (
+      CASE WHEN m.base = lotToken.symbol THEN o.bid / o.lot
+      WHEN m.base = bidToken.symbol THEN o.lot / o.bid
+      ELSE NULL
+      END
+    ) AS price,
+    (
+      CASE WHEN m.base = lotToken.symbol THEN 'ask'
+      WHEN m.base = bidToken.symbol THEN 'bid'
+      ELSE NULL
+      END
+    ) AS type,
+    m.id as market
+  FROM public.offer o
+  LEFT JOIN dict.token lotToken ON LOWER(lotToken.key) = o.lotGem
+  LEFT JOIN dict.token bidToken ON LOWER(bidToken.key) = o.bidGem
+  LEFT JOIN dict.market m
+  ON (lotToken.symbol = m.base AND bidToken.symbol = m.quote)
+  OR (bidToken.symbol = m.base AND lotToken.symbol = m.quote)
+);
+
